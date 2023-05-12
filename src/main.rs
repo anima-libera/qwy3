6e6e3ec7ce405eed850ab02292efcae18ffe261b
anima-libera <anim.libera@gmail.com>
2023-05-12 21:35:28 +0200

    Added window and wgpu minimal setup up to screen clearing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+use winit::{
+	event_loop::{ControlFlow, EventLoop},
+	window::WindowBuilder,
+};
+
 fn main() {
-	println!("Qwy3! In Rust! It's hapenning!");
+	env_logger::init();
+	let event_loop = EventLoop::new();
+	let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+	let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+		backends: wgpu::Backends::all(),
+		dx12_shader_compiler: Default::default(),
+	});
+
+	let window_surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+	// Try to get a cool adapter first.
+	let adapter = instance
+		.enumerate_adapters(wgpu::Backends::all())
+		.find(|adapter| {
+			let info = adapter.get_info();
+			info.device_type == wgpu::DeviceType::DiscreteGpu
+				&& adapter.is_surface_supported(&window_surface)
+		});
+	// In case we didn't found any cool adapter, at least we can try to get a bad adapter.
+	let adapter = adapter.or_else(|| {
+		futures::executor::block_on(async {
+			instance
+				.request_adapter(&wgpu::RequestAdapterOptions {
+					power_preference: wgpu::PowerPreference::HighPerformance,
+					compatible_surface: Some(&window_surface),
+					force_fallback_adapter: false,
+				})
+				.await
+		})
+	});
+	let adapter = adapter.unwrap();
+
+	println!("SELECTED ADAPTER:");
+	dbg!(adapter.get_info());
+	println!("AVAILABLE ADAPTERS:");
+	for adapter in instance.enumerate_adapters(wgpu::Backends::all()) {
+		dbg!(adapter.get_info());
+	}
+
+	let (device, queue) = futures::executor::block_on(async {
+		adapter
+			.request_device(
+				&wgpu::DeviceDescriptor {
+					features: wgpu::Features::empty(),
+					limits: wgpu::Limits::default(),
+					label: None,
+				},
+				None,
+			)
+			.await
+			.unwrap()
+	});
+
+	let size = window.inner_size();
+
+	let surface_caps = window_surface.get_capabilities(&adapter);
+	let surface_format = surface_caps
+		.formats
+		.iter()
+		.copied()
+		.find(|f| f.is_srgb())
+		.unwrap_or(surface_caps.formats[0]);
+	assert!(surface_caps
+		.present_modes
+		.contains(&wgpu::PresentMode::Fifo));
+	let mut config = wgpu::SurfaceConfiguration {
+		usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+		format: surface_format,
+		width: size.width,
+		height: size.height,
+		present_mode: wgpu::PresentMode::Fifo,
+		alpha_mode: surface_caps.alpha_modes[0],
+		view_formats: vec![],
+	};
+	window_surface.configure(&device, &config);
+
+	use winit::event::*;
+	event_loop.run(move |event, _, control_flow| match event {
+		Event::WindowEvent { ref event, window_id } if window_id == window.id() => match event {
+			WindowEvent::CloseRequested
+			| WindowEvent::KeyboardInput {
+				input:
+					KeyboardInput {
+						state: ElementState::Pressed,
+						virtual_keycode: Some(VirtualKeyCode::Escape),
+						..
+					},
+				..
+			} => *control_flow = ControlFlow::Exit,
+			WindowEvent::Resized(new_size) => {
+				config.width = new_size.width;
+				config.height = new_size.height;
+				window_surface.configure(&device, &config);
+			},
+			_ => {},
+		},
+		Event::RedrawRequested(window_id) if window_id == window.id() => {
+			let window_texture = window_surface.get_current_texture().unwrap();
+			let view = window_texture
+				.texture
+				.create_view(&wgpu::TextureViewDescriptor::default());
+			let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+				label: Some("Render Encoder"),
+			});
+
+			encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				label: Some("Render Pass"),
+				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+					view: &view,
+					resolve_target: None,
+					ops: wgpu::Operations {
+						load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.01, g: 0.0, b: 0.05, a: 1.0 }),
+						store: true,
+					},
+				})],
+				depth_stencil_attachment: None,
+			});
+			queue.submit(std::iter::once(encoder.finish()));
+			window_texture.present();
+		},
+		_ => {},
+	});
 }
