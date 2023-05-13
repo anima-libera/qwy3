65b7642ab934870143be0caf7259bad17519fe6e
anima-libera <anim.libera@gmail.com>
2023-05-13 19:19:17 +0200

    Made it to the classic RGB triangle ^^

---
+++ shaders/test_01.wgsl
@@ -0,0 +1,21 @@
+struct VertexInput {
+	@location(0) position: vec3<f32>,
+	@location(1) color: vec3<f32>,
+};
+struct VertexOutput {
+	@builtin(position) clip_position: vec4<f32>,
+	@location(0) color: vec4<f32>,
+};
+
+@vertex
+fn vs_main(vertex_input: VertexInput) -> VertexOutput {
+	var vertex_output: VertexOutput;
+	vertex_output.clip_position = vec4<f32>(vertex_input.position, 1.0);
+	vertex_output.color = vec4<f32>(vertex_input.color, 1.0);
+	return vertex_output;
+}
+
+@fragment
+fn fs_main(the: VertexOutput) -> @location(0) vec4<f32> {
+	return the.color;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use wgpu::util::DeviceExt;
 use winit::{
 	event_loop::{ControlFlow, EventLoop},
 	window::WindowBuilder,
@@ -6,7 +7,10 @@ use winit::{
 fn main() {
 	env_logger::init();
 	let event_loop = EventLoop::new();
-	let window = WindowBuilder::new().build(&event_loop).unwrap();
+	let window = WindowBuilder::new()
+		.with_title("Qwy3")
+		.build(&event_loop)
+		.unwrap();
 
 	let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
 		backends: wgpu::Backends::all(),
@@ -81,6 +85,84 @@ fn main() {
 	};
 	window_surface.configure(&device, &config);
 
+	#[repr(C)]
+	#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+	struct Vertex {
+		position: [f32; 3],
+		color: [f32; 3],
+	}
+
+	let vertices = [
+		Vertex { position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.0] },
+		Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+		Vertex { position: [0.0, 0.5, 0.0], color: [0.0, 0.0, 1.0] },
+	];
+
+	let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+		label: Some("Vertex Buffer"),
+		contents: bytemuck::cast_slice(&vertices),
+		usage: wgpu::BufferUsages::VERTEX,
+	});
+
+	let vertex_buffer_layout = wgpu::VertexBufferLayout {
+		array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+		step_mode: wgpu::VertexStepMode::Vertex,
+		attributes: &[
+			wgpu::VertexAttribute {
+				offset: 0,
+				shader_location: 0,
+				format: wgpu::VertexFormat::Float32x3,
+			},
+			wgpu::VertexAttribute {
+				offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+				shader_location: 1,
+				format: wgpu::VertexFormat::Float32x3,
+			},
+		],
+	};
+
+	let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+		label: Some("Shader"),
+		source: wgpu::ShaderSource::Wgsl(include_str!("../shaders/test_01.wgsl").into()),
+	});
+
+	let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+		label: Some("Render Pipeline Layout"),
+		bind_group_layouts: &[],
+		push_constant_ranges: &[],
+	});
+
+	let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+		label: Some("Render Pipeline"),
+		layout: Some(&render_pipeline_layout),
+		vertex: wgpu::VertexState {
+			module: &shader,
+			entry_point: "vs_main",
+			buffers: &[vertex_buffer_layout],
+		},
+		fragment: Some(wgpu::FragmentState {
+			module: &shader,
+			entry_point: "fs_main",
+			targets: &[Some(wgpu::ColorTargetState {
+				format: config.format,
+				blend: Some(wgpu::BlendState::REPLACE),
+				write_mask: wgpu::ColorWrites::ALL,
+			})],
+		}),
+		primitive: wgpu::PrimitiveState {
+			topology: wgpu::PrimitiveTopology::TriangleList,
+			strip_index_format: None,
+			front_face: wgpu::FrontFace::Ccw,
+			cull_mode: Some(wgpu::Face::Back),
+			polygon_mode: wgpu::PolygonMode::Fill,
+			unclipped_depth: false,
+			conservative: false,
+		},
+		depth_stencil: None,
+		multisample: wgpu::MultisampleState { count: 1, mask: !0, alpha_to_coverage_enabled: false },
+		multiview: None,
+	});
+
 	use winit::event::*;
 	event_loop.run(move |event, _, control_flow| match event {
 		Event::WindowEvent { ref event, window_id } if window_id == window.id() => match event {
@@ -110,18 +192,23 @@ fn main() {
 				label: Some("Render Encoder"),
 			});
 
-			encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-				label: Some("Render Pass"),
-				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-					view: &view,
-					resolve_target: None,
-					ops: wgpu::Operations {
-						load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.01, g: 0.0, b: 0.05, a: 1.0 }),
-						store: true,
-					},
-				})],
-				depth_stencil_attachment: None,
-			});
+			{
+				let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+					label: Some("Render Pass"),
+					color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+						view: &view,
+						resolve_target: None,
+						ops: wgpu::Operations {
+							load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.01, g: 0.0, b: 0.05, a: 1.0 }),
+							store: true,
+						},
+					})],
+					depth_stencil_attachment: None,
+				});
+				render_pass.set_pipeline(&render_pipeline);
+				render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+				render_pass.draw(0..(vertices.len() as u32), 0..1);
+			}
 			queue.submit(std::iter::once(encoder.finish()));
 			window_texture.present();
 		},
