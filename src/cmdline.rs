42916f2eee9e09c1b4018ae64c46ddeec7ff29aa
anima-libera <anim.libera@gmail.com>
2023-12-21 11:11:50 +0100

    Better clap

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -2,69 +2,58 @@ use clap::Parser;
 
 use crate::world_gen::WhichWorldGenerator;
 
-pub struct CommandLineSettings {
-	pub number_of_threads: u32,
-	pub close_after_one_frame: bool,
-	pub verbose: bool,
-	pub output_atlas: bool,
-	pub world_gen_seed: i32,
-	pub which_world_generator: WhichWorldGenerator,
-	pub loading_distance: f32,
-	pub chunk_edge: u32,
-
-	pub test_lang: Option<u32>,
-}
-
 #[derive(Parser)]
-struct ClapParsed {
+#[command(color = clap::ColorChoice::Auto)]
+pub struct CommandLineSettings {
 	/// Number of worker threads in the threadpool.
-	#[arg(long)]
-	threads: Option<u32>,
+	#[arg(long = "threads", short = 't', default_value_t = 12, value_name = "N")]
+	pub number_of_threads: u32,
 
 	/// Does the game should close itself after one frame?
-	#[arg(long)]
-	close: bool,
+	#[arg(long = "close")]
+	pub close_after_one_frame: bool,
 
 	/// Verbose mode. Kinda does nothing for now, sorry >_<.
 	#[arg(long)]
-	verbose: bool,
+	pub verbose: bool,
 
 	/// Outputs the texture atlas as a PNG file.
 	#[arg(long)]
-	output_atlas: bool,
+	pub output_atlas: bool,
 
 	/// World generation seed.
-	#[arg(long)]
-	seed: Option<i32>,
+	#[arg(long = "seed", short = 's', default_value_t = 0, value_name = "SEED")]
+	pub world_gen_seed: i32,
 
-	/*
-	gen: WhichWorldGenerator,
-	*/
+	/// Selection of one world generator.
+	#[arg(
+		long = "gen",
+		short = 'g',
+		value_enum,
+		default_value_t = WhichWorldGenerator::Default,
+		value_name = "GENERATOR_NAME",
+		hide_possible_values = true,
+	)]
+	pub which_world_generator: WhichWorldGenerator,
+
 	/// Loading distance in blocks.
-	#[arg(long)]
-	gen_dist: Option<f32>,
+	#[arg(
+		long = "gen-dist",
+		short = 'd',
+		default_value_t = 190.0,
+		value_name = "LENGTH"
+	)]
+	pub loading_distance: f32,
 
 	/// Length of the edge of the chunks, in blocks.
-	#[arg(long)]
-	chunk_edge: Option<u32>,
+	#[arg(long, default_value_t = 20, value_name = "LENGTH")]
+	pub chunk_edge: u32,
 
 	/// Runs a specific Qwy Script test instead of running the game.
 	#[arg(long)]
-	test_lang: Option<u32>,
+	pub test_lang: Option<u32>,
 }
 
 pub fn parse_command_line_arguments() -> CommandLineSettings {
-	let parsed = ClapParsed::parse();
-
-	CommandLineSettings {
-		number_of_threads: parsed.threads.unwrap_or(12),
-		close_after_one_frame: parsed.close,
-		verbose: parsed.verbose,
-		output_atlas: parsed.output_atlas,
-		world_gen_seed: parsed.seed.unwrap_or(0),
-		which_world_generator: WhichWorldGenerator::Default,
-		loading_distance: parsed.gen_dist.unwrap_or(190.0),
-		chunk_edge: parsed.chunk_edge.unwrap_or(20),
-		test_lang: parsed.test_lang,
-	}
+	CommandLineSettings::parse()
 }
