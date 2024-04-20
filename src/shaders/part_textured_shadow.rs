3caaa89e6a596807fe454930ef2bee2da76caf44
anima-libera <anim.libera@gmail.com>
2024-04-20 20:01:14 +0200

    Properly filled `front_face` and `cull_mode` fields of `PrimitiveState`

--- src/shaders/part_textured_shadow.rs
+++ src/shaders/part_textured_shadow.rs
@@ -80,8 +80,8 @@ pub(crate) fn render_pipeline_and_bind_group(
 		primitive: wgpu::PrimitiveState {
 			topology: wgpu::PrimitiveTopology::TriangleList,
 			strip_index_format: None,
-			front_face: wgpu::FrontFace::Cw,
-			cull_mode: Some(wgpu::Face::Back),
+			front_face: wgpu::FrontFace::Ccw,
+			cull_mode: Some(wgpu::Face::Front),
 			polygon_mode: wgpu::PolygonMode::Fill,
 			unclipped_depth: false,
 			conservative: false,
